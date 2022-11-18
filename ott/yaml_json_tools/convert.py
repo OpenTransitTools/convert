9aae6b8e6aa3d8a9548bc2ef77ce4c5785202997
Frank Purcell <fxpurcell@gmail.com>
2022-11-18 06:23:24 -0800

    fix: run junk added to poetry

--- ott/yaml_json_tools/convert.py
+++ ott/yaml_json_tools/convert.py
@@ -1,7 +1,8 @@
 import yaml
 import json
 
-from pathlib import Path
-conf = yaml.safe_load(Path('data/trip-form/fr.yml').read_text())
-out=json.dumps(conf, indent=2, ensure_ascii=False)
-print(out)
+def yml_to_json():
+    from pathlib import Path
+    conf = yaml.safe_load(Path('data/trip-form/fr.yml').read_text())
+    out=json.dumps(conf, indent=2, ensure_ascii=False)
+    print(out)
