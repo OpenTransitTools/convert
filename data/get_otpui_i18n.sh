52462432ff8c74ce9e78b43a4f5ddee1460fe4cb
Frank Purcell <fxpurcell@gmail.com>
2022-11-30 15:33:19 -0800

    feat(diff): add a diff option to the script, to thus see if .yml locale files have changed since last run

--- data/get_otpui_i18n.sh
+++ data/get_otpui_i18n.sh
@@ -1,27 +1,58 @@
 #
 # copies .yml localization files from OTP-UI to this folder (with a bunch of sub-folders)
 #
-
 DEV_DIR=../..
 OTP_PACKS=$DEV_DIR/otp-ui/packages
 TORA_ENGLISH=$DEV_DIR/trimet.org/src/intl/en.json
 
-cp $TORA_ENGLISH ./
 
-for n in `find $OTP_PACKS | grep i18n.*yml`
-do
-    # create destination sub-dir
-    d=${n##$OTP_PACKS/}
-    d=${d%/i18n*}
-    mkdir -p $d
-
-    # rename the files 
-    rn=${n/$OTP_PACKS}
+function to_relative_file()
+{
+    # turns a ../../blah/path string to a relative ./blah/path string by stripping of characters
+    local rn=$1
+    rn=${rn/$OTP_PACKS}
     rn=${rn/\/i18n}
     rn=${rn/_Hans}
     rn=${rn/-US}
+    echo ".${rn}"
+}
 
-    # copy src to renamed file in sub-dir
-    cp $n .$rn 
-done
 
+function cp_i18n()
+{
+    cp $TORA_ENGLISH ./
+
+    for n in `find $OTP_PACKS | grep i18n.*yml`
+    do
+        # create destination sub-dir
+        d=${n##$OTP_PACKS/}
+        d=${d%/i18n*}
+        mkdir -p $d
+
+        # rename the files
+        rel=$(to_relative_file $n)
+
+        # copy src to renamed file in sub-dir
+        cp $n $rel
+    done
+}
+
+
+function diff_i18n()
+{
+    for n in `find $OTP_PACKS | grep i18n.*yml`
+    do
+        rel=$(to_relative_file $n)
+        diff="diff $n $rel"
+        echo $diff
+        eval $diff
+    done
+}
+
+
+if [[ $1 == "copy" || $1 == "cp" ]]
+then
+    cp_i18n
+else
+    diff_i18n
+fi
