b0cecabbf7a46558a658cc0ab6aed1c7f13cc241
Frank Purcell <fxpurcell@gmail.com>
2023-12-13 18:00:18 -0800

    feat: read csv file

--- ott/convert/csv2json/gtfs.py
+++ ott/convert/csv2json/gtfs.py
@@ -1,6 +1,16 @@
+import os
+import csv
+import inspect
 from ott.utils import file_utils
+
+this_module_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 def feeds_to_json():
     """ 
     """
-    print("HI")
+    file = open(os.path.join(this_module_dir, 'feeds.csv'), 'r')
+    reader = csv.DictReader(file)
+    fn = reader.fieldnames
+    for row in reader:
+        print(row)
+
